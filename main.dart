37149dbdfdd1d8ccde16f45b4ce461abe4eaf68e
Ifeolu06 <msc.ifeoluwa.omotayo@tau.edu.ng>
2024-05-28 14:31:28 +0100

    first commit

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'math:dart';
+
+
+void main(){
+  runApp(MaterialApp(
+       home: Scaffold (
+       backgroundColor: Colors.deepBlue,   
+       )
+  ))
+    }
